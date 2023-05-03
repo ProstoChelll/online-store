ed21af9bceae2a6c78aeff17757f6c61516598d0
Tim-KILLER <abdulmanovtumur@mail.ru>
2023-05-03 10:39:26 +0300

    first commit

---
+++ src/pages/MainPage.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+import { PageTemplate, Header } from "../layouts";
+import { AdBlock } from "../components";
+</script>
+<template>
+  <PageTemplate>
+    <template #header>
+      <Header />
+    </template>
+    <template #ad><AdBlock class="mt-[22px]" /></template>
+    <template #body>
+      <div class="flex-1 h-full bg-white">123123</div>
+    </template>
+  </PageTemplate>
+</template>
---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx,vue}"],
+  theme: {
+    extend: {},
+    screens: {
+      lg: "768px",
+      // => @media (min-width: 640px) { ... }
+
+      md: "1140px",
+      // => @media (min-width: 1024px) { ... }
+
+      xl: "1400px",
+      // => @media (min-width: 1280px) { ... }
+
+      "2xl": "1920px",
+      // => @media (min-width: 1280px) { ... }
+    },
+  },
+  plugins: [],
+};
